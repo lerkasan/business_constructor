c6268a987ed5087950dd8e4f5ef7c2166c19662b
sergii Podoroga <podoroga.mail@gmail.com>
2016-11-07 19:13:09 +0200

    fix login.html

--- src/main/java/ua/com/brdo/business/constructor/Application.java
+++ src/main/java/ua/com/brdo/business/constructor/Application.java
@@ -1,22 +1,12 @@
 package ua.com.brdo.business.constructor;
 
-import org.h2.server.web.WebServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import ua.com.brdo.business.constructor.entity.Role;
-import ua.com.brdo.business.constructor.entity.User;
-import ua.com.brdo.business.constructor.repositories.UsersRepository;
 
-import java.util.*;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"ua.com.brdo.business.constructor.repositories"})

6afcbb42c10d93f8b7f886e1f4b0a2f018936402
mac <mac@macs-MacBook-Pro.local>
2016-11-24 21:35:01 +0200

    reload workflow

--- src/main/webapp/src/vendor.ts
+++ src/main/webapp/src/vendor.ts
@@ -5,9 +5,6 @@ import '@angular/core';
 import '@angular/common';
 import '@angular/http';
 import '@angular/router';
-import 'rxjs';
 import '@angularclass/hmr';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'jquery/dist/jquery.js';
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...

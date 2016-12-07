b9b33f297112394d99b0e34dc61c47e25e8ce725
sergii Podoroga <podoroga.mail@gmail.com>
2016-12-07 19:47:29 +0200

    changed collation in MySQL

--- src/main/resources/sql/create-db-mysql.sql
+++ src/main/resources/sql/create-db-mysql.sql
@@ -1,4 +1,4 @@
 DROP DATABASE business_constructor;
 CREATE DATABASE business_constructor;
-DEFAULT COLLATE utf8_unicode_ci;
+CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 GRANT ALL PRIVILEGES ON business_constructor.* TO 'brdo'@'localhost';

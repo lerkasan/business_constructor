6c7a85f601a74c68327309b4df9aea461b6dfd5a
sergii Podoroga <podoroga.mail@gmail.com>
2016-12-18 21:04:30 +0200

    some minor fixes

--- src/main/java/ua/com/brdo/business/constructor/utils/restsecurity/RESTAuthenticationSuccessHandler.java
+++ src/main/java/ua/com/brdo/business/constructor/utils/restsecurity/RESTAuthenticationSuccessHandler.java
@@ -1,27 +1,17 @@
 package ua.com.brdo.business.constructor.utils.restsecurity;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.ser.FilterProvider;
-import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
-import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import ua.com.brdo.business.constructor.model.Role;
 import ua.com.brdo.business.constructor.model.User;
 
 @Component
@@ -38,7 +28,7 @@ public class RESTAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuc
     }
 
     protected void handle(HttpServletResponse response, Authentication authentication) throws IOException {
-        User user = (User)authentication.getPrincipal();
+        User user = (User) authentication.getPrincipal();
         String userJsonDetails = jsonMapper.writeValueAsString(user);
 
         response.setCharacterEncoding("UTF-8");

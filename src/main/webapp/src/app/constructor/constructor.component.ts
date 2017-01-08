2d3231bfcf16715d5e14e8aacda46630a86aed0b
sergii Podoroga <podoroga.mail@gmail.com>
2017-01-08 19:06:16 +0200

    first commit, constructor prototip

--- src/main/webapp/src/app/constructor/constructor.component.ts
+++ src/main/webapp/src/app/constructor/constructor.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Question} from "../model/question";
+import {Option} from "../model/option";
 
 @Component({
   selector: 'constructor-panel',
@@ -7,10 +9,58 @@ import { Component } from '@angular/core';
 
 })
 
-export class ConstructorComponent{
-	 constructor() {
-  
-  }
-}
+export class ConstructorComponent implements OnInit{
 
+  businesType = '';
+  selectedOption: Option;
+  selectedQuestion: Question;
+  questions: Question[];
+
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  onSelectQuestion(question: Question): void {
+    this.selectedQuestion = question;
+  }
+
+  onSelectOption(option: Option): void {
+    this.selectedOption = option;
+  }
+
+  addQuestion(): void {
+    if(this.questions == undefined){
+      this.questions = [this.newQuestion()];
+    } else {
+      this.questions.push(this.newQuestion());
+    }
+  }
+
+  deleteLastQuestion(): void {
+    this.questions.pop();
+  }
+
+  newQuestion(){
+    let option = new Option();
+    option.title = 'Нова відповідь';
+    let question = new Question();
+    question.text = 'Нове питання';
+    question.options = [option];
+    return question;
+  }
+
+  addOption(options: Option[]): void {
+    let option = new Option;
+    option.title = 'Нова відповідь';
+    options.push(option);
+  }
+
+  deleteLastOption(options: Option[]): void {
+    options.pop();
+  }
+
+}
 

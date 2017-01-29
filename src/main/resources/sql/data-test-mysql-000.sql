19c337007d5457d87dd5bdf6821a59a30047f60b
lerkasan <lerkasan@gmail.com>
2017-01-29 15:52:02 +0200

    added test profile

---
+++ src/main/resources/sql/data-test-mysql-000.sql
@@ -0,0 +1,18 @@
+INSERT INTO role (title) VALUES ('ROLE_USER');
+INSERT INTO role (title) VALUES ('ROLE_ADMIN');
+INSERT INTO role (title) VALUES ('ROLE_EXPERT');
+INSERT INTO role (title) VALUES ('ROLE_MODERATOR');
+
+INSERT INTO user (username, email, password_hash, creation_date) VALUES
+  ('some_user1', 'some_user1@mail.com', '$2a$10$cHVXytfvSSDn5TAm5T/SfedKlSbEzBvFt2Y.AOqgA5jfhIOAlgZ4m',
+   now());
+INSERT INTO user (username, email, password_hash, creation_date) VALUES
+  ('admin', 'some_admin1@mail.com', '$2a$10$V/MgrWzBqwypVcu/RQMPt.iBYQlpHVHycyaGXCM.UwyeOLoSg6VwO',
+   now());
+INSERT INTO user (username, email, password_hash, creation_date) VALUES
+  ('expert', 'some_expert1@mail.com', '$2a$10$U2JdYU2ypsJVS3SFZD8mue68PZLWXSySjrYKtVL44.uZl3KflfXRa',
+   now());
+
+INSERT INTO user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO user_role (user_id, role_id) VALUES (3, 3);
